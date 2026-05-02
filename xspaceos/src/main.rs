bba091332a51ace0688d5707bbe54bdf0621ff6d
Jed Lordy Legaspo <jed.lordy123@gmail.com>
2026-05-03 01:43:47 +0800

    feat: boot kernel with custom x86_64 target bootable x86_64 VGA

--- xspaceos/src/main.rs
+++ xspaceos/src/main.rs
@@ -2,13 +2,24 @@
 #![no_main]
 
 use core::panic::PanicInfo;
-
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO: &[u8] = b"XSpace OS";
+
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    loop {}
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for(i, &byte) in HELLO.iter().enumerate(){
+        unsafe{
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    loop{}
 }
+
